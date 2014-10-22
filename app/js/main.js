7f7a519e5e00c18873c35ed56f52161887e90817
Yaron Miro <yaron@gizra.com>
2014-10-22 06:40:53 +0300

    Update table design.

--- app/js/main.js
+++ app/js/main.js
@@ -15,7 +15,7 @@ jQuery(".navbar-nav li a").click(function() {
 jQuery(document).ready(function(){
   // Display the modal lead form on the pros layout when clicking.
   // Make sure it only execute on mobile.
-  $('.program').find('.title').click(function() {
+  $('.program').find('.extend-button').click(function() {
     $(this).siblings('.description').slideToggle(800);
     $(this).find('i').toggleClass("deg180");
   });
